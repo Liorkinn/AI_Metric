4b08e1d0b3f47bb4dcc3ce5f67cce9b8d2b451bc
Liorkinn <ptkolenie@inbox.ru>
2022-09-23 22:22:21 +0400

    buff

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,11 +1,12 @@
 ﻿import numpy as np
 import pandas as pd
+import warnings
 from sklearn.model_selection import LeaveOneOut
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-
+warnings.filterwarnings("ignore")
 url = "C:/Users/liork/Downloads/machine.data"
 names = [
     "Vendor",
@@ -23,29 +24,26 @@ names = [
 
 data = pd.read_csv(url, names = names)
 
-
-
 X = data.drop(columns=['Vendor','Target','Model', 'ERP'])
 Y = data['Target']
 
 metrics = ['euclidean', 'manhattan', 'chebyshev', 'minkowski']
-print('список метрик:', end = ' ')
-print(str(metrics) + '\nметрика:', end = ' ')
+print('list of metric:', end = ' ')
+print(str(metrics) + '\nmetric - ', end = ' ')
 metricIndex = int(input()) - 1
-print('значение K:', end = ' ')
-k = int(input())
 
-kNeighbors = KNeighborsClassifier(n_neighbors = k, metric = metrics[metricIndex])
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.30, random_state = 1)
 scaler = StandardScaler()
 scaler.fit(X_train)
 X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)
-kNeighbors.fit(X_train,Y_train)
-Y_pred = kNeighbors.predict(X_test)
 
-score = cross_val_score(kNeighbors, X_test, Y_pred)
-print('метрика {}, '.format(metrics[metricIndex]), end = ' ')
-print('коэффициент K - {}, '.format(k), end = ' ')
-print('аккуратность алгоритма {}%'.format(int(round(np.mean(score), 2) * 100)))
 
+for i in range(10):
+    kNeighbors = KNeighborsClassifier(n_neighbors = i+1, metric = metrics[metricIndex])
+    kNeighbors.fit(X_train,Y_train)
+    Y_pred = kNeighbors.predict(X_test)
+    score = cross_val_score(kNeighbors, X_test, Y_pred)
+    print('metric {}, '.format(metrics[metricIndex]), end = ' ')
+    print('coef K - {}, '.format(i+1), end = ' ')
+    print('accuracy {}%'.format(int(round(np.mean(score), 2) * 100)))
